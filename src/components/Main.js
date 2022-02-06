9403d75a64c25c47b26ae71fe72c3399fa8c4c32
xkalf <amroos988@gmail.com>
2022-02-06 15:34:34 +0800

    time fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,7 +20,7 @@ function Main({ displaySec, state, scramble }) {
       <ScrambleContainer>
         <ScrambleText>{scramble}</ScrambleText>
       </ScrambleContainer>
-      <TimeText state={state}>00.00</TimeText>
+      <TimeText state={state}>displaySec</TimeText>
       <StartText>Press and hold Space to start</StartText>
       <Footer>
         <img src={logo} alt="logo" />
